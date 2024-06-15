df74f3d0eb95b88fcb0f3f8c20d13f68ce0ba848
KIRTAN SHAH <skirtan98@gmail.com>
2024-06-15 00:00:23 -0700

    Create vulkan instance

--- VulkanTriangle/VulkanTriangle/main.cpp
+++ VulkanTriangle/VulkanTriangle/main.cpp
@@ -25,7 +25,7 @@ private:
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -34,7 +34,39 @@ private:
         }
     }
 
+    void createInstance() {
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // applcation info
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle Application";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 3, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 3, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_3;
+
+        // instance info
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;  // an outer pointer which points to different inner pointers which point to const characters
+        createInfo.enabledLayerCount = 0;
+
+        // create vulkan instance
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &vulkanInstance);
+        if (vkCreateInstance(&createInfo, nullptr, &vulkanInstance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+        else
+        {
+            std::cout << "Able to create a vulkan instance successfully" << std::endl;
+        }
+    }
+
     void cleanup() {
+        vkDestroyInstance(vulkanInstance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
@@ -46,6 +78,10 @@ private:
 
     // init
     GLFWwindow* window;
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    VkInstance vulkanInstance;
 };
 
 int main() {
